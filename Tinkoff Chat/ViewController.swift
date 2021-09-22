98a262ee7c76f8894ccab6ecbd9b1bac06354cb6
vlad-makukha <vlad.makukha.95@mail.ru>
2021-09-22 11:26:58 +0300

    app lifecycle
    print name of function

---
+++ Tinkoff Chat/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Tinkoff Chat
+//
+//  Created by Владислав Макуха on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print(#function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        print(#function)
+    }
+}
+
