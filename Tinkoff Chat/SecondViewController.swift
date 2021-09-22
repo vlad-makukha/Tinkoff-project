b49b2237441797e02440cd9b39171c9310954953
vlad-makukha <vlad.makukha.95@mail.ru>
2021-09-22 19:45:50 +0300

    update description for the second view

--- Tinkoff Chat/SecondViewController.swift
+++ Tinkoff Chat/SecondViewController.swift
@@ -20,20 +20,20 @@ class SecondViewController: UIViewController {
     // Срабатывает после загрузки View
     override func viewDidLoad() {
         super.viewDidLoad()
-        logFor.log(message: "View moved from init to viewDidLoad")
+        logFor.log(message: "Second View moved from init to viewDidLoad")
         // Do any additional setup after loading the view.
     }
     
     // Срабатывает перед появлением View на экране
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        logFor.log(message: "View moved from viewDidLoad to viewWillAppear")
+        logFor.log(message: "Second View moved from viewDidLoad to viewWillAppear")
     }
     
     // Срабатывает перед тем, как размер View поменяется под размер экрана
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        logFor.log(message: "View moved from viewWillAppear to viewWillLayoutSubviews")
+        logFor.log(message: "Second View moved from viewWillAppear to viewWillLayoutSubviews")
     }
     
     // Тут срабатывает AutoLayout
@@ -41,24 +41,24 @@ class SecondViewController: UIViewController {
     // Срабатывает после того, как размер View изменился под размер экрана
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        logFor.log(message: "View moved from viewWillLayoutSubviews to viewDidLayoutSubviews")
+        logFor.log(message: "Second View moved from viewWillLayoutSubviews to viewDidLayoutSubviews")
     }
     
     // Срабатывает, когда View появляется на экране
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        logFor.log(message: "View moved from viewDidLayoutSubviews to viewDidAppear")
+        logFor.log(message: "Second View moved from viewDidLayoutSubviews to viewDidAppear")
     }
     
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
-        logFor.log(message: "View moved from viewDidAppear to viewWillDisappear")
+        logFor.log(message: "Second View moved from viewDidAppear to viewWillDisappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
-        logFor.log(message: "View moved from viewWillAppear to deinit")
+        logFor.log(message: "Second View moved from viewWillAppear to deinit")
 
     }
     
